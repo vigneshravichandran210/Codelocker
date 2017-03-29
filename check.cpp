7832fc78abb310b76e723fafea2a99fdcf988548
vignesh.R <vickynesh210@gmail.com>
2017-03-29 16:49:43 +0530

    Create check.cpp

---
+++ check.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+long long int n;
+cin>>n;
+if(n==0)
+cout<<"zero";
+else if(n<0)
+cout<<"negative";
+else
+cout<<"positive";
+return 0;
+}
